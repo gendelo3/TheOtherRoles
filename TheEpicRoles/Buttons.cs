843fe0eeba8311fdec23a85b5f96f98f3e3dad32
LaicosVK <vincent.kresse@gmail.com>
2022-04-11 19:28:49 +0200

    Update Buttons.cs

--- TheEpicRoles/Buttons.cs
+++ TheEpicRoles/Buttons.cs
@@ -629,8 +629,13 @@ namespace TheEpicRoles {
                 },
                 () => { return Tracker.tracker != null && Tracker.tracker == PlayerControl.LocalPlayer && !PlayerControl.LocalPlayer.Data.IsDead; },
                 () => {
-                    showTargetNameOnButton(Tracker.currentTarget, trackerTrackPlayerButton, ""); //Show target name under button if setting is true
-                    return PlayerControl.LocalPlayer.CanMove && Tracker.currentTarget != null && !Tracker.usedTracker; },
+                    if (PlayerControl.LocalPlayer.CanMove && Tracker.currentTarget != null && !Tracker.usedTracker)
+                    {
+                        showTargetNameOnButton(Tracker.currentTarget, trackerTrackPlayerButton, ""); //Show target name under button if setting is true
+                        return true;
+                    }
+                    else return false;
+                },
                 () => { if(Tracker.resetTargetAfterMeeting) Tracker.resetTracked(); },
                 Tracker.getButtonSprite(),
                 new Vector3(-1.8f, -0.06f, 0),

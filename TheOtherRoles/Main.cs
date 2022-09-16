c87cf70810806113f56ec58e19e7236ac8aa76e1
Mallöris <|||||@||.||>
2022-09-16 18:42:54 +0200

    readd beta days

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -24,6 +24,7 @@ namespace TheOtherRoles
     {
         public const string Id = "me.eisbison.theotherroles";
         public const string VersionString = "4.1.7";
+        public static uint betaDays = 0;  // amount of days for the build to be usable (0 for infinite!)
 
         public static Version Version = Version.Parse(VersionString);
         internal static BepInEx.Logging.ManualLogSource Logger;

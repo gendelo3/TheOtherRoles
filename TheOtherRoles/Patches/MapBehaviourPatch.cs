9cb2524d409539986abe1e6f19ddf7f2e823d7df
gendelo3 <gendelo3@gmail.com>
2022-10-22 13:06:51 +0200

    remove meeting map position setting from mapbehaviour patch

--- TheOtherRoles/Patches/MapBehaviourPatch.cs
+++ TheOtherRoles/Patches/MapBehaviourPatch.cs
@@ -14,21 +14,6 @@ namespace TheOtherRoles.Patches {
 		private static Dictionary<PlayerControl, SpriteRenderer> herePoints = new Dictionary<PlayerControl, SpriteRenderer>();
 
 		[HarmonyPatch(typeof(MapBehaviour), nameof(MapBehaviour.FixedUpdate))]
-		static bool Prefix(MapBehaviour __instance) {
-			if (!MeetingHud.Instance) return true;  // Only run in meetings, and then set the Position of the HerePoint to the Position before the Meeting!
-			if (!MapUtilities.CachedShipStatus) {
-				return false;
-			}
-			Vector3 vector = AntiTeleport.position != null ? AntiTeleport.position : CachedPlayer.LocalPlayer.transform.position;
-			vector /= MapUtilities.CachedShipStatus.MapScale;
-			vector.x *= Mathf.Sign(MapUtilities.CachedShipStatus.transform.localScale.x);
-			vector.z = -1f;
-			__instance.HerePoint.transform.localPosition = vector;
-			CachedPlayer.LocalPlayer.PlayerControl.SetPlayerMaterialColors(__instance.HerePoint);
-
-			return false;
-		}
-		[HarmonyPatch(typeof(MapBehaviour), nameof(MapBehaviour.FixedUpdate))]
 		static void Postfix(MapBehaviour __instance) {
 			if (Trapper.trapper != null && CachedPlayer.LocalPlayer.PlayerId == Trapper.trapper.PlayerId) {
 				foreach (PlayerControl player in Trapper.playersOnMap) {
